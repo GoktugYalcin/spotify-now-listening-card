7e4cf38ec6ef382ebf524a6f2907146a92ef0f53
A. Göktuğ Yalçın <goktugyalcin06@gmail.com>
2022-01-11 20:35:42 +0300

    Edit section added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/index.scss';
 import { SpotifyCard } from "./SpotifyCard";
+import {Button, Text, TextInput} from "@mantine/core";
+import html2canvas from 'html2canvas';
 
 const App = () => {
-  return (
-      <div className="container">
+    const [name, setName] = useState("A. Göktu Yalçın");
+    const [recentTime, setRecentTime] = useState("2h");
+    const [song, setSong] = useState("25k jacket (feat. Lil Baby)");
+    const [artist, setArtist] = useState("Gunna, Lil Baby");
+    const [playlist, setPlaylist] = useState("En güzel playlist");
+
+    return (
+    <div className="container">
         <div className="content__preview">
-            <SpotifyCard />
+            <SpotifyCard
+                name={name}
+                recentTime={recentTime}
+                song={song}
+                artist={artist}
+                playlist={playlist}
+            />
         </div>
-      </div>
-  );
+
+        <div className="lower">
+            <div className="content__edit">
+                <TextInput
+                    placeholder="Name"
+                    label="Name"
+                    value={name}
+                    styles={{
+                        label: { color: "#293432"}
+                    }}
+                    onChange={(v) => {
+                        setName(v.target.value)
+                    }}
+                />
+                <TextInput
+                    placeholder="Recent Time"
+                    label="Recent Time"
+                    value={recentTime}
+                    styles={{
+                        label: { color: "#293432"}
+                    }}
+                    onChange={(v) => {
+                        setRecentTime(v.target.value)
+                    }}
+                />
+                <TextInput
+                    placeholder="Song"
+                    label="Song"
+                    value={song}
+                    styles={{
+                        label: { color: "#293432"}
+                    }}
+                    onChange={(v) => {
+                        setSong(v.target.value)
+                    }}
+                />
+                <TextInput
+                    placeholder="Artist"
+                    label="Artist"
+                    value={artist}
+                    styles={{
+                        label: { color: "#293432"}
+                    }}
+                    onChange={(v) => {
+                        setArtist(v.target.value)
+                    }}
+                />
+                <TextInput
+                    placeholder="Playlist"
+                    label="Playlist"
+                    value={playlist}
+                    styles={{
+                        label: { color: "#293432"}
+                    }}
+                    onChange={(v) => {
+                        setPlaylist(v.target.value)
+                    }}
+                />
+            </div>
+
+            <div className="content__edit">
+                <Button color="violet"
+                        radius="xl"
+                        size="lg"
+                        onClick={()=>{
+                            html2canvas(document.getElementById('preview')).then((canvas) => {
+                                const data = canvas.toDataURL();
+
+                                const downloadLink = document.createElement("a");
+                                downloadLink.href = data;
+                                downloadLink.download = "SpotifyCard.png";
+                                downloadLink.click();
+                                /*const a = document.createElement("a"); //Create <a>
+                                a.href = "data:image/png;base64," + data; //Image Base64 Goes here
+                                a.download = "Image.png"; //File name Here
+                                a.click(); //Downloaded file*/
+                            })
+                        }}
+                >
+                    Save It
+                </Button>
+                <div className="blank__space"></div>
+                <Text align={"center"} size="lg" weight={700}>If you like it, you can find me on <Text variant="link" variant="gradient"
+                                                                                                       gradient={{ from: 'teal', to: 'indigo', deg: 60 }} component="a" href="https://github.com/GoktugYalcin">here</Text>!</Text>
+            </div>
+        </div>
+    </div>
+    );
 }
 
 export default App;
--- src/components/SpotifyCard.js
+++ src/components/SpotifyCard.js
@@ -5,39 +5,39 @@ import playlistIcon from "../assets/playlistIcon.png";
 import {useSelector} from "react-redux";
 
 
-export const SpotifyCard = () => {
+export const SpotifyCard = ({name, recentTime, song, artist, playlist}) => {
 
-    const name = useSelector((state) => state.config.name)
+    /*const name = useSelector((state) => state.config.name)
     const recentTime = useSelector((state) => state.config.recentTime)
     const song = useSelector((state) => state.config.song)
     const artist = useSelector((state) => state.config.artist)
-    const playlist = useSelector((state) => state.config.playlist)
+    const playlist = useSelector((state) => state.config.playlist)*/
 
     return (
-        <div className="preview">
+        <div id="preview" className="preview">
             <img alt="User Thing" src={userPhoto} className="preview_photo" height="80" width="80"/>
             <div className="preview__info">
                 <div className="preview__info-header">
                     <div className="preview__info-header_name">
-                        {name}
+                        {name.length > 20 ? name.slice(0, 20) : name}
                     </div>
                     <div className="preview__info-header_time">
-                        {recentTime}
+                        {recentTime.length > 4 ? recentTime.slice(0, 4) : recentTime}
                     </div>
                 </div>
                 <div className="preview__info-body">
                     <div className="preview__info-body_song">
-                        {song}
+                        {song.length > 20 ? song.slice(0, 20) + '...' : song}
                     </div>
                     •
                     <div className="preview__info-body_artist">
-                        {artist}
+                        {artist.length > 20 ? artist.slice(0, 20) + '...' : artist}
                     </div>
                 </div>
                 <div className="preview__info-footer">
                     <img alt="Playlist" width="11" height="11" src={playlistIcon} />
                     <div className="preview__info-footer_playlist">
-                        {playlist}
+                        {playlist.length > 20 ? playlist.slice(0, 20) + '...' : playlist}
                     </div>
                 </div>
             </div>
